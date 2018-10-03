d5f35d0dc126603f8c5dadebd5d5465e5aa8b820
Joseph <josephodr@hotmail.com>
2018-10-03 16:29:42 +0100

    Added text file to write query. Using geohive data.

--- src/main/java/remotejenamodel/RemoteJenaMain.java
+++ src/main/java/remotejenamodel/RemoteJenaMain.java
@@ -1,5 +1,13 @@
 package remotejenamodel;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QuerySolution;
@@ -12,29 +20,61 @@ public class RemoteJenaMain {
 
 	public static void main(String[] args) {
 
-		String testQueryString = "PREFIX geo: <http://www.opengis.net/ont/geosparql#>\n"
-				+ "PREFIX geof: <http://www.opengis.net/def/function/geosparql/>\n" + "SELECT ?fWKT \n" + "WHERE{\n"
-				+ "?loc geo:hasGeometry ?geometry .\n" + "?geometry geo:asWKT ?fWKT .\n" + "}";
-		issueSelectQuery(testQueryString);
+		String queryPath = "data/query.txt";
+
+		final Path dataDir = Paths.get(queryPath);
+		if (!Files.isReadable(dataDir)) {
+			System.out.println("Data directory '" + dataDir.toAbsolutePath()
+					+ "' does not exist or is not readable, please check the path");
+			System.exit(1);
+		}
+		StringBuilder query= readFile(dataDir);
+		System.out.println(query.toString());
+		issueSelectQuery(query.toString());
 
 	}
 
-	static void issueSelectQuery(String query) {
+	private static void issueSelectQuery(String query) {
 		QueryExecution exec = QueryExecutionFactory.sparqlService(SERVER_URL, query);
 		ResultSet rs = exec.execSelect();
 		
-		// print out all results in a nice table
-		//ResultSetFormatter.out(rs);
+		String label = "";
+		String wkt = "";
 		
 		// print out results individually
 		while(rs.hasNext()) {
 		    QuerySolution qs = rs.next() ;
-		    System.out.println(qs.getLiteral("fWKT").toString());
+		    label = qs.getLiteral("label").toString();
+		    wkt = qs.getLiteral("wkt").toString();
+		    System.out.println(label + " - " + wkt + "\n\n");
 		}
 		exec.close() ;
-
+	}
+	
+	private static StringBuilder readFile(Path path) {
+		InputStream stream = null;
+		try {
+			stream = Files.newInputStream(path);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
+		String line = null;
+		StringBuilder fileContent = new StringBuilder();
+		BufferedReader in = new BufferedReader(new InputStreamReader(stream));
 
+		// read in file
+		try {
+			while((line = in.readLine()) != null) {
+				fileContent.append(line + "\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Query read successfully...");
+		
+		return fileContent;
 	}
 
 }
